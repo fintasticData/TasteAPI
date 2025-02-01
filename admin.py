3accc77c2d535be9c3154ef6bd37d27a1e9909a3
fintasticData <87995823+fintasticData@users.noreply.github.com>
2025-02-02 01:28:56 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -193,7 +193,7 @@ with tab3:
     with tab6:
         def get_openai_response(prompt):
             load_dotenv()
-            client = OpenAI(api_key=os.getenv("OPENAI05"))
+            client = OpenAI(api_key=os.getenv("MYKEY"))
             #api_key = st.secrets["OPENAI"]
             #client = openai.OpenAI(api_key=api_key)
             try:
