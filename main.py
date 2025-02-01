2e38bb049f67ebe0f12c62aaf14e31190cda4f22
fintasticData <87995823+fintasticData@users.noreply.github.com>
2025-02-01 20:43:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 # Initialize the Generative AI model
 model = genai.GenerativeModel('gemini-pro')
 
-client = OpenAI()
+client = OpenAI(api_key=os.getenv("openai")) 
 
 #Github
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
